1d54a079eef822330407de9feebf5dcc7c2068a1
Raav Koper <rkoper@f1r2s15.codam.nl>
2023-04-17 14:02:49 +0200

    changed ex02 added numfile

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -11,3 +11,51 @@ PmergeMe::~PmergeMe() {}
 PmergeMe&	PmergeMe::operator=( const PmergeMe& other ) {
 	return *this;
 }
+
+void PmergeMe::printDeq() {
+    for (int i = 0; i < deq.size(); i++) {
+        std::cout << deq[i] << " ";
+    }
+    std::endl(std::cout);
+}
+
+void PmergeMe::printVec() {
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << vec[i] << " ";
+    }
+    std::endl(std::cout);
+}
+
+void PmergeMe::fillContainers(int argc, char **argv) {
+    for (int i = 1; i < argc; i++) {
+        try {
+            if (std::stoi(argv[i]) < 0)
+                throw std::invalid_argument("negative integer: " + std::string(argv[i]));
+            vec.push_back(std::stoi(argv[i]));
+            deq.push_back(std::stoi(argv[i]));     
+        } catch (std::exception &e) {
+            std::cerr << e.what() << std::endl;
+            return;
+        }
+    }
+}
+
+void PmergeMe::mergeInsertionSortVec() {
+    for (int i = 1; i < vec.size(); i++) {
+        int j = i;
+        while (j > 0 && vec[j - 1] > vec[j]) {
+            std::swap(vec[j], vec[j - 1]);
+            j--;
+        }
+    }
+}
+
+void PmergeMe::mergeInsertionSortDeq() {
+    for (int i = 1; i < deq.size(); i++) {
+        int j = i;
+        while (j > 0 && deq[j - 1] > deq[j]) {
+            std::swap(deq[j], deq[j - 1]);
+            j--;
+        }
+    }
+}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,9 +1,30 @@
 #include "PmergeMe.hpp"
 
-int main(int argc, char **argv) {
-	if (argc < 2) {
-		std::cerr << "Usage: " << argv[0] << " <sequence of positive integers>" << std::endl;
-		return 1;
+int main(int argc, char	**argv) {
+    if (argc < 2) {
+		std::cout << "Usage: ./main <sequence of positive integers>" << std::endl;
+		return 0;
 	}
-	PmergeMe pmergeMe;
+	PmergeMe p;
+
+	p.fillContainers(argc, argv);
+
+	std::cout << "Unsorted sequence:" << std::endl;
+	p.printVec();
+
+	std::clock_t startVec = std::clock();
+    p.mergeInsertionSortVec();
+	std::clock_t endVec = std::clock();
+    
+	std::clock_t startDeq = std::clock();
+    p.mergeInsertionSortDeq();
+	std::clock_t endDeq = std::clock();
+    
+    std::cout << "Sorted sequence:" << std::endl;
+	p.printVec();
+
+	std::cout << "Time taken by vector: " << (endVec - startVec) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
+	std::cout << "Time taken by deque: " << (endDeq - startDeq) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
+        
+    return 0;
 }
